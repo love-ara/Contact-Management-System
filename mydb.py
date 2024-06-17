032cb66bdeede645b1145f64ca3ef94f66a24b60
Ara <aramideotenaike@gmail.com>
2024-06-17 12:30:58 +0100

    commit

--- mydb.py
+++ mydb.py
@@ -3,7 +3,7 @@ import mysql.connector
 dataBase = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "imotua26"
+    password = ""
 )
 
 cursorObject = dataBase.cursor()
