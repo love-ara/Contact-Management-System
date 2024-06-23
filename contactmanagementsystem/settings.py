99a0931b1ad08f66e5a180d51a10f8b9ba7ab5e6
miriamemmanuel21 <miriamemmanuel075@gmail.com>
2024-06-23 20:08:50 +0100

    update functionality

--- contactmanagementsystem/settings.py
+++ contactmanagementsystem/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'contact_management_system',
         'USER': 'root',
-        'PASSWORD': 'Business@1',
+        'PASSWORD': 'Password',
         'HOST': 'localhost',
         'PORT': '3306'
     }
